3c0a34bd8e2cb7583dc6f2f9c082503245ea2b23
Christoph Linhart <christoph.linhart@bmw.de>
2022-01-28 10:21:29 +0100

    Inital commit

---
+++ terraform/main.tf
@@ -0,0 +1,55 @@
+provider "azurerm" {
+  tenant_id       = "${var.tenant_id}"
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  features {}
+}
+terraform {
+  backend "azurerm" {
+    storage_account_name = "tfstate14536"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    resource_group_name  = "tfstate"
+    access_key           = "OhTFkiiWuh2CGRWGocfd3xJ9BhG2b4UA/2dtCgNpGTjOr8A0SzQ3/ggicpMZvnI1OrddBsiYBX2HFl8FOtPhWg=="
+  }
+}
+module "resource_group" {
+  source               = "./modules/resource_group"
+  resource_group       = "${var.resource_group}"
+  location             = "${var.location}"
+}
+module "network" {
+  source               = "./modules/network"
+  address_space        = "${var.address_space}"
+  location             = "${var.location}"
+  virtual_network_name = "${var.virtual_network_name}"
+  application_type     = "${var.application_type}"
+  resource_type        = "NET"
+  resource_group       = "${module.resource_group.resource_group_name}"
+  address_prefix_test  = "${var.address_prefix_test}"
+}
+
+module "nsg-test" {
+  source           = "./modules/networksecuritygroup"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "NSG"
+  resource_group   = "${module.resource_group.resource_group_name}"
+  subnet_id        = "${module.network.subnet_id_test}"
+  address_prefix_test = "${var.address_prefix_test}"
+}
+module "appservice" {
+  source           = "./modules/appservice"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "AppService"
+  resource_group   = "${module.resource_group.resource_group_name}"
+}
+module "publicip" {
+  source           = "./modules/publicip"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "publicip"
+  resource_group   = "${module.resource_group.resource_group_name}"
+}
